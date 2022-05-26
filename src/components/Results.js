df31ed6786190771b7fe6dbe23bdfe1d04deb7e5
Ricardo Juárez Tepos <45005831+RichieSjt@users.noreply.github.com>
2022-05-26 12:25:49 -0500

    Update Results.js
    Co-Authored-By: Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>

--- src/components/Results.js
+++ src/components/Results.js
@@ -12,10 +12,10 @@ const Results = ({ data={} }) => {
     return (
         <div className="results-container">
             <div className='results'>
-                <p>Average clients in the queue (L<sub>q</sub>): {Lq} { Lq && 'clients'}</p>
-                <p>Average clients in the system (L): {L} {L && 'clients'}</p>
-                <p>Wait time in queue (W<sub>q</sub>): {Wq} {Wq && unit}</p>
-                <p>Average time in the system (W): {W} {W && unit}</p>
+                <p>Average clients in the queue (L<sub>q</sub>): {Lq} clients</p>
+                <p>Average clients in the system (L): {L} clients</p>
+                <p>Wait time in queue (W<sub>q</sub>): {Wq} {unit}</p>
+                <p>Average time in the system (W): {W} {unit}</p>
                 {rho && <p>Rho (ρ): {rho}</p>}
                 {lambda_e && <p>Effective arrival rate (λ<sub>e</sub>): {lambda_e} clients/{lambda_e && unit.slice(0, -1)}</p>}
                 {totalCost && <p>Total cost (C<sub>T</sub>): ${totalCost}</p>}
